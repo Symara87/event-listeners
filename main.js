6b7f2ed419fd7695d748c66bd06fe29fcc22aac3
Symara Kaslov <symarakaslov@Symaras-MacBook-Pro.local>
2016-06-08 20:51:16 -0500

    one down

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 // GO!
 
+var navMenuNode = document.querySelector(".nav-menu")
+//found menu node//
+var buttonNode = document.querySelector("#navButton")
+//found button node//
+
+var navMenuState = {
+ menuShowing: true
+}
+
+var toggle = function(){
+	if (navMenuState.menuShowing === true) {
+		navMenuNode.style.opacity = 0
+		navMenuState.menuShowing = false 
+	}
+	else{
+		navMenuNode.style.opacity = 1
+		navMenuState.menuShowing = true
+	}
+
+}
+
+buttonNode.addEventListener("click",toggle)
+
+
